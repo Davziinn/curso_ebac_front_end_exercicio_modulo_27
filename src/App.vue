277b1318664dece597bd25b400ddb5837795cc55
Davi <mdavi6085@gmail.com>
2024-08-27 15:49:49 -0300

    enviando o exercicio do modulo 27

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <div class="calculator">
+      <input v-model.number="num1" type="number" placeholder="Digite o primeiro número">
+      <input v-model.number="num2" type="number" placeholder="Digite o segundo número">
+      <select v-model="operation">
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+      </select>
+    </div>
+    <div class="result">
+      <p>
+        Resultado: {{ result }}
+      </p>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      operation: '+'
+    };
+  },
+  computed: {
+    result() {
+      switch (this.operation) {
+        case '+':
+          return this.num1 + this.num2;
+        case '-':
+          return this.num1 - this.num2;
+        case '*':
+          return this.num1 * this.num2;
+        case '/':
+          return this.num2 !== 0 ? this.num1 / this.num2 : 'Divisão por zero';
+        default:
+          return 0;
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.calculator {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+}
+input, select {
+  margin: 10px;
+  padding: 10px;
+  font-size: 16px;
+}
+.result {
+  margin-top: 20px;
+  font-size: 18px;
+  font-weight: bold;
+}
+</style>
